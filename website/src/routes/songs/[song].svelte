764055264640ed6a7f98e7b3b200c6ca8c6a23e0
Marc Jamot <marcjamot@gmail.com>
2021-06-24 22:22:00 +0200

    Center song text

--- website/src/routes/songs/[song].svelte
+++ website/src/routes/songs/[song].svelte
@@ -23,29 +23,36 @@
 </script>
 
 <div class="song">
-  <div class="title">
-    <h3>{song.title}</h3>
-  </div>
+  <div>
+    <div class="title">
+      <h3>{song.title}</h3>
+    </div>
 
-  <div class="metadata">
-    Melody: <span>{song.melody}</span><br />
-    Language: <span>{song.lang}</span>
-  </div>
+    <div class="metadata">
+      Melody: <span>{song.melody}</span><br />
+      Language: <span>{song.lang}</span>
+    </div>
 
-  <div class="text">
-    {#each song.text as line}
-      {#if line === ""}
-        <br />
-      {:else}
-        <p>{line}</p>
-      {/if}
-    {/each}
+    <div class="text">
+      <div>
+        {#each song.text as line}
+          {#if line === ""}
+            <br />
+          {:else}
+            <p>{line}</p>
+          {/if}
+        {/each}
+      </div>
+    </div>
   </div>
 </div>
 
 <style>
   .song {
     margin: 16px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
   .title {
     font-size: 1.5rem;
@@ -54,5 +61,8 @@
     margin-top: 16px;
     font-size: 1.2rem;
     line-height: 1.8rem;
+  }
+  .text > div {
+    display: block;
   }
 </style>
