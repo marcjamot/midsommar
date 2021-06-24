67a7eb365f2c44a1217e2e8c33b02cfbffa25552
Marc Jamot <marcjamot@gmail.com>
2021-06-24 20:47:37 +0200

    Add drinks

--- website/src/routes/drinks/index.svelte
+++ website/src/routes/drinks/index.svelte
@@ -1,1 +1,28 @@
-Drinks
+<script context="module" lang="ts">
+  interface Drink {
+    title: string;
+    booze: string;
+  }
+
+  import type { Load } from "@sveltejs/kit";
+
+  export const load: Load = async ({ fetch }) => {
+    const drinks: Drink[] = await (await fetch("/drinks.json")).json();
+    drinks.sort((a, b) => a.title.localeCompare(b.title));
+
+    return {
+      props: {
+        drinks: drinks,
+      },
+    };
+  };
+</script>
+
+<script lang="ts">
+  export let drinks: Drink[];
+</script>
+
+{#each drinks as drink}
+  <h3>{drink.title}</h3>
+  <p>{drink.booze}</p>
+{/each}
